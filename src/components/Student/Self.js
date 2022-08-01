b474a177db4459f66effae4a9f49752c754cddd9
ArtMejia <arteniomejia.student@careerdevs.com>
2022-08-01 18:44:54 -0400

    updated self

--- src/components/Student/Self.js
+++ src/components/Student/Self.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Container from '../common/Container';
+import Splash from '../common/Splash';
+import homesplash from '../../assets/homesplash.jpg';
+
 
 const Self = () => {
     return (
-        <h1>Self</h1>
+        <Container>
+            <Splash image={homesplash} style={{color: "#010101"}}>
+                <h1 style={{textShadow : '1px 1px white'}}>Self</h1>
+            </Splash>
+        </Container>
     )
 }
 
